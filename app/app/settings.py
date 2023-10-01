dc6f40b755367c6ff4b7792bf2360c4e78297405
Serhiі Stupnytskyi <stupns@macPro.attlocal.net>
2023-10-01 14:51:27 -0400

    add swagger and url path for api documentation .

--- app/app/settings.py
+++ app/app/settings.py
@@ -27,6 +27,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'rest_framework',
+    'drf_spectacular',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +120,7 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'core.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
