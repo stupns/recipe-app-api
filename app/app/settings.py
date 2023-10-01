9be27a4bb0d002bf579d2987701c99c2f8b607c5
Serhiі Stupnytskyi <stupns@macPro.attlocal.net>
2023-10-01 10:28:06 -0400

    fix settings.py .

--- app/app/settings.py
+++ app/app/settings.py
@@ -70,7 +70,6 @@ DATABASES = {
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASS'),
-        'PORT': '5432',
     }
 }
 
