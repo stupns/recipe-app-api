e65c165b93a22888d2773e51079e53cf6e0864e8
Serhiі Stupnytskyi <stupns@macPro.attlocal.net>
2023-10-13 18:44:45 -0400

    Builds tags API.

--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -11,10 +11,10 @@ from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register('recipes', views.RecipeViewSet)
+router.register('tags', views.TagViewSet)
 
 app_name = 'recipe'
 
 urlpatterns = [
     path('', include(router.urls)),
-
 ]
